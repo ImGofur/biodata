765c3e58c60ff26a8660361cf77925517c53150c
Gofurxml <Gofurkindom@gmail.com>
2024-01-10 21:15:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,251 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="App">
+   
+<div class="hero_area">
+<div class="hero_bg_box">
+  <div class="bg_img_box">
+    <img src="finexo-html/images/hero-bg.png" alt="" />
+  </div>
+</div>
+
+
+<header class="header_section">
+  <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg custom_nav-container ">
+      <a class="navbar-brand" href="index.html">
+        <span>
+         PORTOVOLIO
+        </span>
+      </a>
+
+      {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class=""> </span>
+      </button> */}
+
+      {/* <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav  ">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.html"> About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="why.html">Riwayat</a>
+          </li>
+        </ul>
+      </div> */}
+    </nav>
+  </div>
+</header>
+
+<section class="slider_section ">
+  <div id="customCarousel1" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="container ">
+          <div class="row">
+            <div class="col-md-6 ">
+              <div class="detail-box">
+                <h1>
+                  Hallo I,m <br/>
+                  M Abdul Gofur
+                </h1>
+              </div>
+              <h2>
+              <a href="https://www.facebook.com/@gofur" target="_blank">
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+            </a>
+
+            <a href="https://wa.me/082132848129" target="_blank">
+             <i class="fa fa-whatsapp" aria-hidden="true"></i>
+            </a>
+            <a href="https://github.com/nama_pengguna_github" target="_blank">
+            <i class="fa fa-github" aria-hidden="true"></i>
+          </a>
+
+            <a  href="https://www.instagram.com/Gofurxml_/" target="_blank">
+              <i class="fa fa-instagram" aria-hidden="true"></i>
+            </a>
+            <a href="https://www.youtube.com/@gofur3502" target="_blank">
+             <i class="fa fa-youtube-play" aria-hidden="true"></i>
+            </a>
+              </h2>
+            </div>
+            <div class="col-md-6">
+              <div class="img-box">
+                <img src="finexo-html/images/red_eyes_naruto_uzumaki_keren_sasuke_uchiha_gambar_hd_naruto-1920x1080.jpg
+                " alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
     </div>
+  </div>
+</section>
+</div>
+
+<section id="about" class="about_section layout_padding">
+  <div class="container">
+    <div class="heading_container heading_center">
+      <h2>
+        Abo<span>ut</span>
+      </h2>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="img-box">
+          <img src="finexo-html/images/red_eyes_naruto_uzumaki_keren_sasuke_uchiha_gambar_hd_naruto-1920x1080.jpg" alt="" />
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="detail-box">
+          <p><br/>
+            Saya Adalah Siswa RPL di sekolah SMKN 6 JEMBER
+          </p>
+          <p>
+            Nama   : Mohammad Abdul Gofur
+          </p>
+          <p>
+            Status : Siswa
+          </p>
+          <p>
+            Phone  : +62 089*****
+          </p>
+          <p>
+            Umur   : 18 Tahun
+          </p>
+          <p>
+            Alamat : Gambirono
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+<section class="why_section layout_padding">
+<div class="container">
+  <div class="heading_container heading_center">
+    <h2>
+      Riwayat
+    </h2>
+    <h2>
+      Pendi<span>dikan</span>
+    </h2>
+  </div>
+  <div class="why_container">
+    <div class="box">
+      <div class="img-box">
+        <img src="finexo-html/images/logo-TK-Indonesia-223073_961x675.png" alt="" />
+      </div>
+      <div class="detail-box">
+        <h5>
+          TK HARAPAN BANGSA
+        </h5>
+        <p>
+         2008-2011
+         <p>
+         </p>
+         Lokasi : Gambirono Bangsalsari
+        </p>
+      </div>
+    </div>
+    <div class="box">
+      <div class="img-box">
+        <img src="finexo-html/images/png-transparent-jember-wirowongso-regency-wonoasri-logo-others-miscellaneous-cdr-emblem.png" alt="" />
+      </div>
+      <div class="detail-box">
+        <h5>
+          SDN GAMBIRONO 04
+        </h5>
+        <p>
+         2011-2017
+         </p>
+         <p>
+         Lokasi :Gambirono Bangsalsari
+        </p>
+      </div>
+    </div>
+    <div class="box">
+      <div class="img-box">
+        <img src="finexo-html/images/887752b5a8e25b13075bf8860d40c313.jpg" alt="" />
+      </div>
+      <div class="detail-box">
+        <h5>
+         MTSN 04 JEMBER
+        </h5>
+        <p>
+         2017-2020 
+         </p>
+         <p>
+         Lokasi :Gambirono Bangsalsari
+        </p>
+      </div>
+    </div>
+    <div class="box">
+      <div class="img-box">
+        <img src="finexo-html/images/SMKN 6 JEMBER 1080px (COLOR).png" alt="" />
+      </div>
+      <div class="detail-box">
+        <h5>
+          SMK NEGERI 6 JEMBER
+        </h5>
+        <p>
+         2020-2024 
+         <p>
+         </p>
+         Lokasi :Tanggul
+        </p>
+      </div>
+    </div>
+  </div>
+  
+</div>
+</section>
+
+
+<section class="team_section layout_padding">
+<div class="container-fluid">
+
+  <div class="team_container">
+    <div class="row">
+      <div class="col-lg-3 col-sm-6">
+        {/* <div class="box ">  
+          <div class="social_box">
+          <a href="https://www.facebook.com/@gofur" target="_blank">
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+            </a>
+
+            <a href="https://wa.me/082132848129" target="_blank">
+             <i class="fa fa-whatsapp" aria-hidden="true"></i>
+            </a>
+            <a href="https://github.com/nama_pengguna_github" target="_blank">
+            <i class="fa fa-github" aria-hidden="true"></i>
+          </a>
+
+            <a  href="https://www.instagram.com/Gofurxml_/" target="_blank">
+              <i class="fa fa-instagram" aria-hidden="true"></i>
+            </a>
+            <a href="https://www.youtube.com/@gofur3502" target="_blank">
+             <i class="fa fa-youtube-play" aria-hidden="true"></i>
+            </a>
+
+          </div>
+        </div> */}
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+
+</div>
+
   );
 }
 
